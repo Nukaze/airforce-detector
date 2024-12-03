750863be53eab4b6bb297d4dd2687496b54acc1a
Nukaze <nukaze4@gmail.com>
2024-12-03 19:39:14 +0700

    feat: title tap `icon` and `name`
    - add icon from `streamlit docs`, `google font icon`, `emojicopy`

--- src/app.py
+++ src/app.py
@@ -1,7 +1,26 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import keras
+import tensorflow as tf
+
+
+def pre_config() -> None:
+    print("\nstreamlit version: ",st.__version__)
+    
+    city_sunrise_icon = ":city_sunrise:"
+    graph_icon = "📈"
+    # ref: https://fonts.google.com/icons?icon.query=graph
+    graph_monitoring_google_icon = """<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFF55"><path d="M160-120q-17 0-28.5-11.5T120-160v-40q0-17 11.5-28.5T160-240q17 0 28.5 11.5T200-200v40q0 17-11.5 28.5T160-120Zm160 0q-17 0-28.5-11.5T280-160v-220q0-17 11.5-28.5T320-420q17 0 28.5 11.5T360-380v220q0 17-11.5 28.5T320-120Zm160 0q-17 0-28.5-11.5T440-160v-140q0-17 11.5-28.5T480-340q17 0 28.5 11.5T520-300v140q0 17-11.5 28.5T480-120Zm160 0q-17 0-28.5-11.5T600-160v-200q0-17 11.5-28.5T640-400q17 0 28.5 11.5T680-360v200q0 17-11.5 28.5T640-120Zm160 0q-17 0-28.5-11.5T760-160v-360q0-17 11.5-28.5T800-560q17 0 28.5 11.5T840-520v360q0 17-11.5 28.5T800-120ZM560-481q-16 0-30.5-6T503-504L400-607 188-395q-12 12-28.5 11.5T131-396q-11-12-10.5-28.5T132-452l211-211q12-12 26.5-17.5T400-686q16 0 31 5.5t26 17.5l103 103 212-212q12-12 28.5-11.5T829-771q11 12 10.5 28.5T828-715L617-504q-11 11-26 17t-31 6Z"/></svg>"""
+    analytics_google_icon = """<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFF55"><path d="M280-280h80v-200h-80v200Zm320 0h80v-400h-80v400Zm-160 0h80v-120h-80v120Zm0-200h80v-80h-80v80ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z"/></svg>"""
+    
+    st.set_page_config(
+        page_title="Stock Lumina",
+        page_icon=graph_monitoring_google_icon,
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
+    return
+
 
 
 def main() -> None:
@@ -13,5 +32,6 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    pre_config()
     main()
     ...
